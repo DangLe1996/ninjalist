875b4041e0a555076a0834ee0856d92589fdeda8
DangLe1996 <baodang153@gmail.com>
2021-07-12 13:06:42 -0400

    init commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere nostrum, accusantium voluptatem, architecto aperiam perspiciatis adipisci unde culpa error asperiores officia sed iure fugiat ullam molestiae, veritatis magni molestias praesentium!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere nostrum, accusantium voluptatem, architecto aperiam perspiciatis adipisci unde culpa error asperiores officia sed iure fugiat ullam molestiae, veritatis magni molestias praesentium!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere nostrum, accusantium voluptatem, architecto aperiam perspiciatis adipisci unde culpa error asperiores officia sed iure fugiat ullam molestiae, veritatis magni molestias praesentium!</p>
+        </div>   
+    );
+}
+ 
+export default About;
